a7a86d319e925dfe39d0f2d031ce40b3ead15b0b
maxdavid <me@maxdavid.com>
2019-05-25 11:34:32 -0700

    Add card component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
-import './Card.css';
+import './Card.scss';
+
+const CardContent = () => {
+  return (
+    <div className='card-caption'>
+      <div className='card-title'>Get started with React</div>
+      <div className='card-description'>Yada whatevr</div>
+      <div className='card-base-url'>reactjs.org</div>
+    </div>
+  );
+};
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -8,7 +8,7 @@ const HeaderContent = () => {
     <div className="header-content">
       <HeaderTitle />
       <span className='post-text'>
-        My lifestyle is funky, and I want to plant a tree. We need backwards whipped cream, man. #slickbus #randomtweet
+        My lifestyle is funky, and I want to plant a tree. We need backwards whipped cream, man. #slickbus #randomtweetMy lifestyle is funky, and I want to plant a tree. 
       </span>
     </div>
   )
